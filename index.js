2f18d4e63f26a409de7d99a04342ca3d577c3601
JureKotnik <jure2002k@gmail.com>
2023-04-18 12:52:05 +0200

    not efficient version

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) =>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
+    }
+    if(req.url === '/api/users'){
+        const users = [
+            { name: 'Bob Smith', age: 40},
+            { name: 'John Doe', age: 30}
+        ];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(users));
+    }
+});
 
-logger.on('message', (data) => console.log('Called Listener', data));
+const PORT = process.env.PORT || 5000;
 
-logger.log('Hello world');
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
